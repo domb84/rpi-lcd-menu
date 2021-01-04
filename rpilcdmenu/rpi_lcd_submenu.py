bc0f0f6e71c250dcba781ac17ea833d8e0ba8613
Dominic Bird <dominic@dominicbird.com>
2021-01-04 22:29:54 +0000

    - clear queue on new message - incremement release - remove unnecessary prints - move render function - add scrolling_menu option to submenu

--- rpilcdmenu/rpi_lcd_submenu.py
+++ rpilcdmenu/rpi_lcd_submenu.py
@@ -2,9 +2,10 @@ from rpilcdmenu import RpiLCDMenu
 
 
 class RpiLCDSubMenu(RpiLCDMenu):
-    def __init__(self, base_menu, scrolling_menu=False):
+    def __init__(self, base_menu, scrolling_menu):
         """
         Initialize SubMenu
         """
         self.lcd = base_menu.lcd
+
         super(RpiLCDMenu, self).__init__(base_menu, scrolling_menu)
